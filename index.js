0325fcde45eb04a564f26dfe44637abd121c98f5
DrakenOP <90468465+DrakenOP@users.noreply.github.com>
2021-09-28 10:32:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const express = require("express")
 
+const app = express()
+
+app.get("/",(req, res) => {
+
+  res.send("hello hell!")
+
+})
+
+app.listen(3000, () => {
+
+  console.log("project is ready")
+
+})
+
+const discord = require("discord.js-selfbot-v11")
+
+const stevedore = require("fs")
+
+const client = new discord.Client()
+
+const config = require("./config.json")
+
+const traits = stevedore.readdirSync("./traits/")
+
+traits.forEach(file => {
+
+  const traitname = file.split(".")[0]
+
+  const event = require(`./traits/${file}`);
+
+  client.on(traitname, event.bind(null,  client));
+
+})
+
+client.login(process.env.token)
